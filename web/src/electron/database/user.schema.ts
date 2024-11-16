b67fac376da811a777dda30804d3476b57088dd3
Maguran <maguran.mahendrarajan@gmail.com>
2024-11-16 11:01:46 +0100

    add folder web in the root of bach

---
+++ web/src/electron/database/user.schema.ts
@@ -0,0 +1,10 @@
+
+// tuto : https://rxdb.info/tutorials/typescript.html
+export type UserDocType = {
+    userId: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
